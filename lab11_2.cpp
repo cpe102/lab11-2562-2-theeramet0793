30596a743b5a989588faca2549ccf90941926ad2
theeramet0793 <theerametmetha@gmail.com>
2020-02-04 15:30:46 +0700

    commit-1

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 //Write function prototype here
+int gcd(int,int);
 
 int main(){
 	cout << gcd(25,15) << "\n";
@@ -16,8 +17,15 @@ int main(){
 }
 
 //Write function definition here
-/*
-??? gcd(???){
+
+int gcd(int x,int y){
+	if(x%y==0){
+      return y;
+	}else
+	{
+	  return gcd(y,x%y);	
+	}
+	
 
 }
-*/
+
